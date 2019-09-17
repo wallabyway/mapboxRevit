d562d4b42aea15d148f9e7c76d36e9cd9ab346db
MICHAEL BEALE <bealester@gmail.com>
2019-09-16 23:39:33 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# mapboxRevit
-View Revit models inside Mapbox 
+# mapbox Revit
+View Revit models inside Mapbox, and click on Revit properties.
+
+<img src="https://user-images.githubusercontent.com/440241/65015225-beaaa980-d8d5-11e9-94ce-4876c628ac09.gif" width="100%>
+
+### Notes
+
+- Click on the model to view properties (via the Forge API. Alternatively, use props.db sqlite DB file instead)
+- Use svf-extract to convert Revit file to glTF (see glTF folder)
+
+
+### Bugs
+- Currently the mapbox raycaster does not match correctly, so it cannot get the correct DBID from the gltf.node.name
+
+
+![mapbox-static](https://user-images.githubusercontent.com/440241/65015291-ef8ade80-d8d5-11e9-99e9-d3a5d221f6ca.jpg)
+
+
+### Other examples of MapBox with Revit data:
+
+- Perkins-Will : http://research.perkinswill.com
+- Archistar.AI : http://archistar.ai
+- Ridley-Willow : https://www.willowinc.com
+
+### References
+
+- geo-location in LMV blog: https://forge.autodesk.com/blog/mini-map-geolocation-extension
+- design Automation for Revit (rooms and Spaces): https://github.com/wallabyway/rooms-spaces-revit-plugin
+- https://github.com/wallabyway/propertyServer/blob/master/pipeline.md
+
+## Property SQLite Database usage
+
+The demo is currently querying Forge service to retrieve meta-data given the dbID selected.
+
+Alternatively, you can use the `props.db` sqlite database file, located in the folder `/gltf`.  Load the file in sqlite and query for properties associated with the dbID you click on.
+
+### Example
+
+We will replicate the property panel in Forge Viewer.
+
+![properties](https://user-images.githubusercontent.com/440241/65016868-2a8f1100-d8da-11e9-9461-1f4905ca679b.jpg)
+
+1. We selected the 'wall' and it's DBID is 2594.  I used `NOP_VIEWER.getSelection()` to get the value.
+
+
+2. Now open props.db file in a SQLite browser.
+
+3. To filter on `ids.id = 2594` use this SQL command:
+
+```
+select ids.external_id, attrs.name, attrs.category, attrs.data_type, vals.value from _objects_eav eavs
+left join _objects_id ids on ids.id = eavs.entity_id 
+left join _objects_attr attrs on attrs.id = eavs.attribute_id
+left join _objects_val vals on vals.id = eavs.value_id where ids.id = 2594 order by eavs.entity_id
+```
+
+4.  This returns the results:
+
+```
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	parent	__parent__	11	2107
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	instanceof_objid	__instanceof__	11	2107
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Level	__internalref__	11	5
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Location Line	Constraints	20	Wall Centerline
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Base Constraint	Constraints	20	Level 2
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Base Offset	Constraints	3	-500.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Base is Attached	Constraints	1	0.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Base Extension Distance	Constraints	3	0.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Top Constraint	Constraints	20	Up to level: Roof Line
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Unconnected Height	Constraints	3	3500.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Top Offset	Constraints	3	0.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Top is Attached	Constraints	1	0.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Top Extension Distance	Constraints	3	0.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Room Bounding	Constraints	1	1
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Related to Mass	Constraints	1	0.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Structural	Structural	1	0.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Enable Analytical Model	Structural	1	0.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Structural Usage	Structural	20	Non-bearing
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Length	Dimensions	3	19702.0
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Area	Dimensions	3	43.2957000000004
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Volume	Dimensions	3	8.15002486322177
+c85e5be0-d8d5-4148-836f-b55e711ef373-00068ac9	Type Name	Identity Data	20	SIP 202mm Wall - conc clad
+
+```
+
+...and matches what's in the Forge viewer's property panel (see above).
+
---
+++ docs/app.js
@@ -0,0 +1,195 @@
+'use strict';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXhhbXBsZXMiLCJhIjoiY2p0MG01MXRqMW45cjQzb2R6b2ptc3J4MSJ9.zA2W0IkI0c6KaAhJfk9bWg';
+
+// parameters to ensure the model is georeferenced correctly on the map
+let modelAltitude = 0;
+let modelRotate = [0, 0, -Math.PI / 10];
+let modelOrigin = [-71.059505, 42.349448];  //use gltf/output.metadata.json -> metadata.georeference.positionLL84[lat,long]
+const modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude);
+
+
+// Class to load a glTF file into mapBox
+// https://docs.mapbox.com/mapbox-gl-js/example/add-3d-model/
+// ----------------------------------------------------------
+
+class glTFLayer {
+    constructor(modelAsMercatorCoordinate) {
+        this.raycaster = new THREE.Raycaster();
+		this.mouse = new THREE.Vector2();
+		this.toast = document.getElementById('toast')
+
+        this.id = '3d-model';
+        this.type = 'custom';
+        this.renderingMode = '3d';
+
+        // transformation parameters to position, rotate and scale the 3D model onto the map
+        this.modelTransform = {
+            translateX: modelAsMercatorCoordinate.x,
+            translateY: modelAsMercatorCoordinate.y,
+            translateZ: modelAsMercatorCoordinate.z,
+            rotateX: modelRotate[0],
+            rotateY: modelRotate[1],
+            rotateZ: modelRotate[2],
+            // Since our 3D model is in real world meters, a scale transform needs to be applied since the CustomLayerInterface expects units in MercatorCoordinates.
+            // 0.2 is a scale factor that should come from LMV
+            scale: 0.2 * modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
+        };
+    }
+
+    showPropertiesFromForge(id) {
+    	const forgeURL = 'https://developer.api.autodesk.com';
+    	const urn = "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dnJwYXJ0eTEvcmFjX2FsbHZpZXdzMy5ydnQ";
+    	const guid = "6c9aa7c5-0799-1c3c-a715-3ed2aafa21ab";
+    	const token = _adsk.token.access_token;
+    	fetch(`${forgeURL}/modelderivative/v2/designdata/${urn}/metadata/${guid}/properties`,
+    		{ mode: 'cors', headers: { Accept: 'application/json', Authorization:`Bearer ${token}` }})
+    		.then( res => res.json())
+    		.then( res => {
+    			//let prop = (res.data.collection.filter(i=>{return i.objectid==id}))[0];
+                // Choose a random DBId, since the raycaster is broken :-(
+    			let prop = res.data.collection[Math.round(Math.random()*100)];
+        		toast.innerHTML = `properties:[ ${prop.name} ]`;
+			    toast.classList.add('show');
+    			clearTimeout(this.tmout) 
+	        	this.tmout = setTimeout(function(){ toast.classList.remove('show') }, 3000);
+    		});
+    }
+
+    onClick(e) {
+		if (!this.camera) return;
+    	this.mouse.x = ( e.originalEvent.clientX / window.innerWidth ) * 2 - 1;
+		this.mouse.y = - ( e.originalEvent.clientY / window.innerHeight ) * 2 + 1;
+		this.raycaster.setFromCamera( this.mouse, this.camera );
+	    let id = this.raycaster.intersectObjects(this.scene.children);
+	    this.showPropertiesFromForge(2891);
+
+        // set bbox as 5px reactangle area around clicked point
+        //var bbox = [[e.point.x - 5, e.point.y - 5], [e.point.x + 5, e.point.y + 5]];
+    }
+
+    onAdd(map, gl) {
+        this.camera = new THREE.PerspectiveCamera();
+        this.scene = new THREE.Scene();
+
+        // create two three.js lights to illuminate the model
+        var directionalLight = new THREE.DirectionalLight(0xffffff);
+        directionalLight.position.set(0, -70, 100).normalize();
+        this.scene.add(directionalLight);
+
+        var directionalLight2 = new THREE.DirectionalLight(0xffffff);
+        directionalLight2.position.set(0, 70, 100).normalize();
+        this.scene.add(directionalLight2);
+
+        // use the three.js GLTF loader to add the 3D model to the three.js scene
+        var loader = new THREE.GLTFLoader();
+        loader.load('gltf/output.gltf', (function(gltf) {
+            this.scene.add(gltf.scene);
+        }).bind(this));
+        this.map = map;
+
+        // use the Mapbox GL JS map canvas for three.js
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: map.getCanvas(),
+            context: gl,
+            antialias: true
+        });
+
+        this.renderer.autoClear = false;
+    }
+
+    render(gl, matrix) {
+        var rotationX = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(1, 0, 0), this.modelTransform.rotateX);
+        var rotationY = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 1, 0), this.modelTransform.rotateY);
+        var rotationZ = new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0, 0, 1), this.modelTransform.rotateZ);
+        var m = new THREE.Matrix4().fromArray(matrix);
+        var l = new THREE.Matrix4().makeTranslation(this.modelTransform.translateX, this.modelTransform.translateY, this.modelTransform.translateZ)
+            .scale(new THREE.Vector3(this.modelTransform.scale, -this.modelTransform.scale, this.modelTransform.scale))
+            .multiply(rotationX)
+            .multiply(rotationY)
+            .multiply(rotationZ);
+
+        this.camera.projectionMatrix.elements = matrix;
+        this.camera.projectionMatrix = m.multiply(l);
+        this.renderer.state.reset();
+        this.renderer.render(this.scene, this.camera);
+    }
+}
+
+
+// Class to display 3D extruded buildings and fly to position
+// https://docs.mapbox.com/mapbox-gl-js/example/3d-buildings/
+// https://docs.mapbox.com/mapbox-gl-js/example/flyto-options/
+// ----------------------------------------------------------
+
+class buildingsLayer {
+    constructor() {
+        this.id = '3d-buildings';
+        this.source = 'composite';
+        this['source-layer'] = 'building';
+        this.filter = ['==', 'extrude', 'true'];
+        this.type = 'fill-extrusion',
+        this.minzoom = 15,
+        this.paint = {
+            'fill-extrusion-color': '#aaa',
+
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            'fill-extrusion-height': [
+                "interpolate", ["linear"],
+                ["zoom"],
+                15, 0,
+                15.05, ["get", "height"]
+            ],
+            'fill-extrusion-base': [
+                "interpolate", ["linear"],
+                ["zoom"],
+                15, 0,
+                15.05, ["get", "min_height"]
+            ],
+            'fill-extrusion-opacity': .6
+        };
+	}
+
+	flyToHome() {
+		map.flyTo({ center: modelOrigin, zoom: 21, speed: 0.7 });
+	}
+
+    onAdd(map, gl) {	
+	    // Insert the layer beneath any symbol layer.
+	    var layers = map.getStyle().layers;
+
+	    var labelLayerId;
+	    for (var i = 0; i < layers.length; i++) {
+	        if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+	            labelLayerId = layers[i].id;
+	            break;
+	        }
+	    }        
+    }
+}
+
+
+
+// create our mapbox with our 3d model as center
+// ----------------------------------------------------------
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 18,
+    center: modelOrigin,
+    pitch: 60,
+    antialias: true // create the gl context with MSAA antialiasing, so custom layers are antialiased
+});
+
+
+// Load 3D glTF model
+const gltf = new glTFLayer( modelAsMercatorCoordinate );
+map.on('load', function() { map.addLayer(gltf) });
+map.on('click', function(e) { gltf.onClick(e) });
+
+
+// Load 3D extruded buildings as layer
+const buildings = new buildingsLayer();
+map.on('load', function() { map.addLayer(buildings) });
